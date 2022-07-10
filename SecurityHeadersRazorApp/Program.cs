a04528755abbb2c2228717fa6c0f67ffe7af112c
NeutrinoSunset <18445931+Neutrino-Sunset@users.noreply.github.com>
2022-07-10 11:02:33 +0100

    Added Piranha.

--- SecurityHeadersRazorApp/Program.cs
+++ SecurityHeadersRazorApp/Program.cs
@@ -1,25 +1,81 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
+using Piranha;
+using Piranha.AspNetCore.Identity.SQLite;
+using Piranha.Data.EF.SQLite;
+
+
+
+// This initialization adds the specified security headers to all requests.
+/*
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseKestrel(option => option.AddServerHeader = false);
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
    app.UseExceptionHandler("/Error");
-   // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
+app.Use(async (context, next) =>
+{
+   context.Response.Headers.Add("Content-Security-Policy", "default-src 'self';");
+   context.Response.Headers.Add("X-XSS-Protection", "1; mode=block");
+   context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+   context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
+   await next.Invoke();
+});
+
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
+app.MapRazorPages();
+
+app.Run();
+
+*/
+
+// This minimal Piranha initialization adds the security headers to page requests, but not
+// to the requests for any static resources.
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddPiranha(options =>
+{
+   options.UseCms();
+   options.UseFileStorage(naming: Piranha.Local.FileStorageNaming.UniqueFolderNames);
+   options.UseEF<SQLiteDb>(db =>
+       db.UseSqlite("Filename=./PiranhaWeb.db"));
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+   app.UseExceptionHandler("/Error");
+   app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.Use(async (context, next) =>
+{
+   context.Response.Headers.Add("Content-Security-Policy", "default-src 'self';");
+   context.Response.Headers.Add("X-XSS-Protection", "1; mode=block");
+   context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+   context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
+   await next.Invoke();
+});
 
 app.MapRazorPages();
+
+app.UsePiranha(options =>
+{
+   App.Init(options.Api);
+});
 
 app.Run();
